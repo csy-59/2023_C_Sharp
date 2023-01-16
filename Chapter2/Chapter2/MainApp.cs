80b497e276e449f4d7541c4c5ca9847372daede6
csy-59 <csy010509@naver.com>
2023-01-16 13:37:10 +0900

    [Chapter2] 연습 문제 1까지

---
+++ Chapter2/Chapter2/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Chapter2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("사용법: Chapter2.exe <이름>");
+                return;
+            }
+
+            WriteLine("Hello {0}!", args[0]);
+            WriteLine($"===${nameof(Practice1)}===");
+            Practice1();
+        }
+
+        static void Practice1()
+        {
+            WriteLine("여러분, 안녕하세요?\n반갑습니다!");
+        }
+    }
+}
